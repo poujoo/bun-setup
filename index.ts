48fbb7bc832b27e7ba3501ad4fceccfe40737307
vagrant <vagrant@kujiranode>
2023-10-16 08:54:27 +0000

    bun-server

--- index.ts
+++ index.ts
@@ -1,1 +1,28 @@
-console.log("Hello via Bun!");
+import figlet from "figlet";
+const server = Bun.serve({
+    port:3000,
+    fetch(req){
+        const url = new URL(req.url)
+        if(url.pathname === '/'){
+            const body = figlet.textSync("Ciao Bun")
+            return new Response(body)
+        }
+        if(url.pathname === '/about'){
+            return new Response("About me")
+        }
+
+        //handle Error
+        if(url.pathname === '/feed'){
+            throw new Error('Could not fetch!')
+        }
+        return new Response('404!')
+    },
+    error(error){
+        return new Response(`<pre>${error} \n ${error.stack}</pre>`,{
+            headers:{
+                'Content-Type':'text-html'
+            }
+        })
+    }
+})
+console.log(`On port ${server.port}`);
